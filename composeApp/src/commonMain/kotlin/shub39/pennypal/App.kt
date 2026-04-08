0d9daacac8b49b497c8eebd6502d2b3b29d2e721
shub39 <cptnshubham39@gmail.com>
2026-04-08 11:50:50 +0530

    feat: added bottom navigation

--- composeApp/src/commonMain/kotlin/shub39/pennypal/App.kt
+++ composeApp/src/commonMain/kotlin/shub39/pennypal/App.kt
@@ -1,7 +1,12 @@
 package shub39.pennypal
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShortNavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -13,19 +18,25 @@ import androidx.savedstate.serialization.SavedStateConfiguration
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
+import pennypal.composeapp.generated.resources.Res
+import pennypal.composeapp.generated.resources.add
+import pennypal.composeapp.generated.resources.analytics
+import pennypal.composeapp.generated.resources.home
+import pennypal.composeapp.generated.resources.settings
 import shub39.pennypal.presentation.fadeTransitionMetadata
 import shub39.pennypal.presentation.home.HomePage
 import shub39.pennypal.presentation.theme.AppTheme
 import shub39.pennypal.viewmodel.HomeViewModel
 
 @Serializable
-sealed interface AppRoutes {
-    @Serializable data object HomePage : NavKey
+sealed interface AppRoutes: NavKey {
+    @Serializable data object HomePage : AppRoutes
 
-    @Serializable data object AnalyticsPage : NavKey
+    @Serializable data object AnalyticsPage : AppRoutes
 
-    @Serializable data object SettingsPage : NavKey
+    @Serializable data object SettingsPage : AppRoutes
 }
 
 val appConfig = SavedStateConfiguration {
@@ -43,7 +54,47 @@ fun App() {
     AppTheme {
         val globalBackStack = rememberNavBackStack(appConfig, AppRoutes.HomePage)
 
-        Scaffold { padding ->
+        Scaffold(
+            bottomBar = {
+                NavigationBar {
+                    listOf(
+                        AppRoutes.HomePage,
+                        AppRoutes.AnalyticsPage,
+                        AppRoutes.SettingsPage
+                    ).forEach { route ->
+                        NavigationBarItem(
+                            selected = globalBackStack.last() == route,
+                            onClick = {
+                                globalBackStack.removeAll { it == route }
+                                globalBackStack.add(route)
+                            },
+                            icon = {
+                                Icon(
+                                    painter = painterResource(
+                                        when (route) {
+                                            AppRoutes.AnalyticsPage -> Res.drawable.analytics
+                                            AppRoutes.HomePage -> Res.drawable.home
+                                            AppRoutes.SettingsPage -> Res.drawable.settings
+                                        }
+                                    ),
+                                    contentDescription = null
+                                )
+                            },
+                            label = {
+                                Text(
+                                    text = when (route) {
+                                        AppRoutes.AnalyticsPage -> "Analytics"
+                                        AppRoutes.HomePage -> "Home"
+                                        AppRoutes.SettingsPage -> "Settings"
+                                    }
+                                )
+                            },
+                            alwaysShowLabel = true
+                        )
+                    }
+                }
+            }
+        ) { padding ->
             NavDisplay(
                 modifier = Modifier.padding(padding),
                 backStack = globalBackStack,
@@ -54,6 +105,14 @@ fun App() {
                             val state by viewmodel.state.collectAsStateWithLifecycle()
 
                             HomePage(state = state, onAction = viewmodel::onAction)
+                        }
+
+                        entry<AppRoutes.AnalyticsPage> {
+
+                        }
+
+                        entry<AppRoutes.SettingsPage> {
+
                         }
                     },
             )
